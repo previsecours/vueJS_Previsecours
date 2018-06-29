e17e12a73d1793b907c1863329b7c0bdf7a993d2
Guillim <guillaume.lancrenon@commoprices.com>
2018-06-29 18:37:01 +0200

    ajout des fontawsome dqns ce nouveau mode webpack

--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ import brands from '@fortawesome/fontawesome-free-solid'
 import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner'
 fontawesome.library.add(brands, faSpinner)
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 //so that we can user momentJS in every vues: with {{ $moment() }} ou this.moment()
 import VueMomentLib from 'vue-moment-lib';
 Vue.use(VueMomentLib);
