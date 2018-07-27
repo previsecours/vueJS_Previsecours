55aebe29c6d70ff4a40ed6c28effee24b0f08938
Guillim <guillaume.lancrenon@commoprices.com>
2018-07-27 12:24:01 +0200

    on finit l'esthetique du boutton d export de la donnee. reste plus que la mise en forme de la donnee a linterieur du CSV

--- src/components/ButtonExport.vue
+++ src/components/ButtonExport.vue
@@ -1,16 +1,14 @@
 <template>
-    <vue-json-to-csv id="ButtonExport" :json-data=data csv-title="previsecours_predictions"></vue-json-to-csv>
+    <JsonToCsv id="ButtonExport" :json-data=data csv-title="previsecours_predictions" :icon="iconName"></JsonToCsv>
 </template>
 
 <script>
-import buttonprint from './reusable/Button.vue'
 import es from '../store/elasticsearch.js'
-import VueJsonToCsv from 'vue-json-to-csv'
+import JsonToCsv from './reusable/JsonToCsv.vue'
 
 export default {
   components: {
-    buttonprint,
-    VueJsonToCsv
+    JsonToCsv
   },
   data () {
     return {
@@ -57,40 +55,6 @@ export default {
 
 <style>
 /* we cannot "scoped" this style because we want to overwrite the default style of the vue componenet VueJsonToCsv */
-    #ButtonExport button {
-        width: 40px;
-        height: 40px;
-        position: relative;
-        margin: 0px 0px;
-        display: inline-block;
-        vertical-align: middle;
-        background-color: #f6f6f6 !important;
-        text-align: center;
-        color: #636b6f !important;
-        padding: 10px;
-        text-transform: none;
-        font-weight: 300;
-        border: 0;
-        background-image: linear-gradient(#009688, #009688), linear-gradient(#D2D2D2, #D2D2D2) !important;
-        background-size: 0 2px, 100% 0px !important;
-        background-repeat: no-repeat !important;
-        background-position: center bottom, center calc(100% - 1px) !important;
-        transition: background 0s ease-out !important;
-        float: none !important;
-        box-shadow: none;
-        border-radius: 0;
-        list-style: none;
-        &:hover {
-          background: #e1e1e1 !important;
-          cursor: pointer !important;
-        }
-        a {
-            &:hover {
-                text-decoration: none;
-            }
-        }
-    }
-
   #ButtonExport{
     position: fixed;
     top: 60px;
---
+++ src/components/reusable/JsonToCsv.vue
@@ -0,0 +1,157 @@
+<template>
+  <span v-on:click="handleClick" :id="'json-to-csv-' + _uid">
+    <buttonprint v-bind:iconName="icon"></buttonprint>
+  </span>
+</template>
+
+<script>
+import buttonprint from './Button.vue'
+
+export default {
+  components: {
+    buttonprint,
+  },
+  props: {
+    jsonData: {
+      type: Array,
+      required: true
+    },
+    csvTitle: {
+      type: [String, Number],
+      default: 'csv',
+      required: false
+    },
+    showLabels: {
+      type: Boolean,
+      default: true,
+      required: false
+    },
+    labels: {
+      type: Object,
+      required: false
+    },
+    icon: {
+      type: String,
+      required: false
+    },
+  },
+  data: () => ({
+    csvLabels: null,
+    csvData: null,
+  }),
+  destroyed () {
+    this.csvLabels = null
+    this.csvData = null
+  },
+  methods: {
+    handleClick () {
+      let hasErrorEvent = (Object.keys(this._events).indexOf('error') > -1)
+      let hasSuccessEvent = (Object.keys(this._events).indexOf('success') > -1)
+      if (!this.jsonData.length) {
+        this.handleError(`Error: Data are empty`, hasErrorEvent)
+        return
+      }
+      if (this.labels && !Object.keys(this.labels).length) {
+        this.handleError(`Error: Labels are empty`, hasErrorEvent)
+        return
+      }
+      let labels = Object.getOwnPropertyNames({ ...this.jsonData[0] })
+      let labelsConf = this.labels || this.$_createCsvLabelsConf(labels)
+      this.csvLabels = this.showLabels ? this.$_createCsvLabels(labelsConf) : ''
+      this.csvData = this.$_createCsvContent(this.jsonData, labelsConf)
+      if (this.csvLabels === 'error' || this.csvData === 'error') {
+        this.handleError(`Error: An error occured while parsing the data.`, hasErrorEvent)
+        return
+      }
+      let content = this.csvLabels + this.csvData
+      let f = this.$_downloadCsv(`csv-${this._uid}`, content, this.csvTitle)
+      if (!f) this.handleError(`An error has occured`, hasErrorEvent)
+      if (f && hasSuccessEvent) this.$emit('success', true)
+    },
+    handleError (msg, hasErrorEvent) {
+      throw msg
+      if (hasErrorEvent) this.$emit('error', msg)
+    },
+    $_createCsvLabelsConf (arr) {
+      let conf = {}
+
+      arr.map(function (m, i) {
+        conf[m] = { title: m }
+      })
+
+      return conf
+    },
+    $_createCsvLabels (labels) {
+      let row = ''
+      let c = ''
+
+      try {
+        Object.keys(labels).map(function (m, i) {
+          row += '"' + labels[m].title + '",'
+        })
+
+        row = row.slice(0, -1)
+
+        c += row + '\r\n'
+      } catch (err) {
+        c = 'error'
+      } finally {
+        return c
+      }
+    },
+    $_createCsvContent (arr, labels) {
+      let row = ''
+      let c = ''
+      let type = ''
+
+      try {
+        arr.map(function (m, i) {
+          row = ''
+
+          Object.keys(labels).map(function (k, s) {
+            type = typeof m[k]
+            if (type === 'number' || type === 'float') {
+              row += m[k] + ','
+            } else {
+              row += '"' + m[k] + '",'
+            }
+          })
+
+          row = row.slice(0, -1)
+
+          c += row + '\r\n'
+        })
+      } catch (err) {
+        c = 'error'
+      } finally {
+        return c
+      }
+    },
+    $_downloadCsv (uid, csv, title) {
+      try {
+        let uri = 'data:text/csv;charset=utf-8,' + encodeURI(csv)
+
+        let link = document.createElement('a')
+
+        link.id = 'csv-' + uid
+        link.href = uri
+
+        document.body.appendChild(link)
+
+        document.getElementById(link.id).style.visibility = 'hidden'
+        document.getElementById(link.id).download = title + '.csv'
+
+        document.body.appendChild(link)
+        document.getElementById(link.id).click()
+
+        setTimeout(function () {
+          document.body.removeChild(link)
+        })
+        return true
+      } catch (err) {
+        return false
+      }
+    },
+  }
+}
+</script>
